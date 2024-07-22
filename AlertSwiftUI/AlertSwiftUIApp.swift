4d7fbac7623660cbb04a215eb20af0cc54bd39ec
Meltem <meltemak8@gmail.com>
2024-07-22 15:18:56 +0200

    Initial Commit

---
+++ AlertSwiftUI/AlertSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertSwiftUIApp.swift
+//  AlertSwiftUI
+//
+//  Created by Meltem Uğurlu on 22.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AlertSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
