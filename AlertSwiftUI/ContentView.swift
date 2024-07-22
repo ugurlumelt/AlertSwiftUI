69b83677e770d6c1f1154238995b2fb1b3216476
Meltem <meltemak8@gmail.com>
2024-07-22 15:44:19 +0200

    Alert

--- AlertSwiftUI/ContentView.swift
+++ AlertSwiftUI/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var alert = false
+    @State private var actionSheetAlert = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (spacing: 100) {
+            Button("Alert"){
+                alert = true
+            }.alert("Title", isPresented: $alert, actions: {
+                Button("Cancel", role: .cancel, action: {
+                    print("chosen cancel")
+                })
+                Button("Okey", role: .destructive, action: {
+                    print("Okey")
+                })
+                
+            }, message:  {Text("İndex") })
+                    
+            Button("Action Sheet"){
+                actionSheetAlert = true
+            }.actionSheet(isPresented: $actionSheetAlert) {
+             ActionSheet(title: Text("Title"),
+                        message: Text("New Index"),
+                         buttons: [
+                            .default(Text("Cancel"),action: {
+                                print("cancel")
+                            }),
+                            .destructive(Text("Okey"), action: {
+                              print("Okey")
+                            })
+                         ])
+            }
+            
+            
+            Text("Context Menu") //when long press
+                .contextMenu {
+                    Button{
+                        print("fotography chosen")
+                    }label: {
+                        Label("Take a photo", systemImage: "camera")
+                    }
+                    Button{
+                        print("video chosen")
+                    }label: {
+                        Label("Save video", systemImage: "video")
+                    }
+                }
         }
-        .padding()
     }
 }
 
